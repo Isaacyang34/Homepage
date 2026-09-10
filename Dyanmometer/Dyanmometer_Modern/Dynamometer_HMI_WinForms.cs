278e4a2af4723d72f1ac9bf1da0df35aebc5cb7b
Isaacyang34 <isaac@example.com>
2026-09-11 07:48:06 +0800

    Release: Dynamometer HMI V2.5.0 auto-packaged 2026-09-11 07:48

--- Dyanmometer/Dyanmometer_Modern/Dynamometer_HMI_WinForms.cs
+++ Dyanmometer/Dyanmometer_Modern/Dynamometer_HMI_WinForms.cs
@@ -26,8 +26,8 @@ using System.Diagnostics;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.7.2.0")]
+[assembly: AssemblyFileVersion("2.7.2.0")]
 
 namespace DynamometerHMI
 {
